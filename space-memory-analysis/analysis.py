2c4dee823936c858a6a533d53fed45bd1bb490fa
Phu Dang <dangnphu31@gmail.com>
2023-05-23 10:52:48 -0700

    working on space memory analysis

--- space-memory-analysis/analysis.py
+++ space-memory-analysis/analysis.py
@@ -5,6 +5,9 @@ import plotly.express as px
 import seaborn as sns
 import os 
 import pickle
+import sys
+from util import *
+import gzip
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -13,3 +16,66 @@ path = 'C:/Users/phuro/UCSD/NiemaLab/data/'
 fp = os.path.join(path, 'covid19variants.csv')
 df = pd.read_csv(fp)
 
+# Creating a dictionary of variants (keys are variant names & values are 
+# subset dataframes of variants)
+variants = df['variant_name'].unique()
+variants_dict = {var: df[df['variant_name'] == var] for var in variants}
+
+# Calculating rolling averages for each variant dataframes in dictionary 
+for key, val in variants_dict.items():
+    val.reset_index(inplace=True)
+    calculate_rolling_avg(val, 5, 3)
+    val.set_index('index', inplace=True)
+
+# Concatenating the variant-specific dataframes into one 
+data = pd.concat(variants_dict.values())
+data = data.dropna(subset=['date'])
+
+# Export to data folder
+wp = os.path.join(path, 'covid19-variants-w-rolling-avg.csv')
+data.to_csv(wp)
+
+results = open('results.txt', 'w')
+
+# Get memory size of resulting dataset object/file
+object_size = sys.getsizeof(data)
+result = f'Memory size of dataset object: {object_size} bytes'
+print(result); results.write(result + '\n')
+
+file_size = os.stat(wp).st_size
+result = f'Memory size of dataset file: {file_size} bytes\n'
+print(result); results.write(result + '\n')
+
+# Pickle dataset
+wp = os.path.join(path, 'covid19-variants-w-rolling-avg.pickle')
+with open(wp, 'wb') as f:
+    pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+# Get memory size of resulting dataset pickle file
+with open(wp, 'rb') as f:
+    df_from_pickle = pickle.load(f)
+obj_size_pkl = sys.getsizeof(df_from_pickle)
+result = f'Memory size of dataset object (pickled): {obj_size_pkl} bytes'
+print(result); results.write(result + '\n')
+
+file_size_pkl = os.stat(wp).st_size
+result = f'Memory size of pickle file: {file_size_pkl} bytes\n'
+print(result); results.write(result + '\n')
+
+# Compress pickle file with gzip
+wp = os.path.join(path, 'covid19-variants-w-rolling-avg.pkl.gz')
+with gzip.open(wp, 'wb') as f:
+    pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+# Get memory size of compressed pickle file
+with gzip.open(wp, 'rb') as f:
+    df_pkl_zipped = pickle.load(f)
+obj_size_gzipped = sys.getsizeof(df_pkl_zipped)
+result = f'Memory size of dataset object (pickled & zipped): {obj_size_gzipped} bytes'
+print(result); results.write(result + '\n')
+
+file_size_gzipped = os.stat(wp).st_size
+result = f'Memory size of gzip file: {file_size_gzipped} bytes\n'
+print(result); results.write(result + '\n')
+
+results.close()
---
+++ space-memory-analysis/util.py
@@ -0,0 +1,50 @@
+import os
+import pandas as pd
+import numpy as np
+
+
+# Function for calculating rolling averages using queue data structure
+def calculate_rolling_avg(dataframe, column_idx, num_rows):
+
+  """
+  Calculate rolling averages of the data field at 'column_idx' by averaging
+  'num_rows' rows prior and after.
+
+  """
+
+  averages = []
+  q = []
+  total_rows = dataframe.shape[0]
+  data = list(dataframe.iloc[:, column_idx])
+  max_size = num_rows*2+1
+  pointer = 0
+  pointer_offset = 0
+
+  for i in range(total_rows):
+    if i < num_rows:
+      q.append(data[i])
+      values = data[:i] + data[i: i+num_rows+1]
+    elif len(q) != max_size and i < max_size:
+      q.append(data[i])
+      pointer_offset += 1
+      if len(q) == max_size:
+        pointer = i - pointer_offset
+      continue
+    elif i == total_rows-1:
+      averages.append(np.mean(q))
+      del q[0]
+      q.append(data[i])
+      for j in range(pointer_offset):
+        values = data[pointer-num_rows:]
+        averages.append(np.mean(values))
+      continue
+    else:
+      values = q.copy()
+      del q[0]
+      q.append(data[i])
+      pointer += 1
+    averages.append(np.mean(values))      
+
+  dataframe['rolling_avg'] = averages
+  
+  return None
